64338fbd7e1e877ee872b430064405a53f950cf3
Kristian Ceder <kristianceder@gmail.com>
2023-04-20 09:40:02 +0200

    added fade resume for pointpillars

---
+++ configs/pointpillars/hv_pointpillars_fpn_sbn-all_4x8_2x_nus-3d_fade_resume.py
@@ -0,0 +1,7 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_fpn_nus.py',
+    '../_base_/datasets/nus-3d.py', '../_base_/schedules/schedule_2x.py',
+    '../_base_/default_runtime.py'
+]
+
+resume_from = '/mimer/NOBACKUP/groups/snic2021-7-127/enliden/databasesamplerv2/mmdetection3d/work_dirs_pp_no_fade/epoch_15.pth'
